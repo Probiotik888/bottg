133b65654d2ad072e7b3c924ff1274b03e426a03
Probiotik888 <79096947+Probiotik888@users.noreply.github.com>
2023-10-28 23:30:35 +0300

    Add files via upload

---
+++ ProjectBot/src/main/java/ru/emelyanovs/config/BotConfig.java
@@ -0,0 +1,7 @@
+package ru.emelyanovs.config;
+
+import lombok.Data;
+
+@Data
+public class BotConfig {
+}
---
+++ ProjectBot/src/main/java/ru/emelyanovs/config/BotInitializer.java
@@ -0,0 +1,22 @@
+package ru.emelyanovs.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.emelyanovs.bot.TelegramBot;
+
+@Slf4j
+public class BotInitializer {
+
+    TelegramBot bot;
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        }
+        catch (TelegramApiException e) {
+            log.error("Ошибка: " + e.getMessage());
+        }
+    }
+}
